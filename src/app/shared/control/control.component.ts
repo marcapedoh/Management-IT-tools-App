1e359fc0979bac76a6e3e1d1e60c1b1f5fbb414e
marcapedoh <adralabel@gmail.com>
2024-11-25 07:44:08 +0000

    contentChild vs viewChild

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -15,8 +15,11 @@ import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '
 export class ControlComponent {
   @Input({ required: true }) label!: string;
   @HostBinding('class') className = 'control';
+
+  @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
   @HostListener('click') onClick() {
     console.log('Clicked');
+    console.log(this.control)
   };
 
 
